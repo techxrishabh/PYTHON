bde04cadc98c008f85f5ab23a06bae213375e400
rishabh22204901 <rishabh22204901@gmail.com>
2023-08-10 20:51:11 +0530

    basics-of-python done

--- CHAPTER - 1/hello_world.py
+++ CHAPTER - 1/hello_world.py
@@ -9,9 +9,17 @@ print("Hello-World")
 # 1.   '/t' - Tab:
 #     When used in a string, it inserts a tab character
 
+print('Inserting a tab character in string of python by using slash t')
 print('Hello\tWorld')
 
 # 2. '\n' - Newline
 #     When it is use in string it creates a line break
 
+print('giving a line break by using slash n')
 print('Hello\nWorld')
+
+# Two asterisks (**) in python represent exponents
+
+print('asterisks-in-python')
+value = 2 ** 4
+print(value)
