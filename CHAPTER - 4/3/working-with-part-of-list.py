bf949e20ffdbcd7eba26b200c4d2a995a4aec37a
rishabh22204901 <rishabh22204901@gmail.com>
2023-08-25 15:16:52 +0530

    slicing a list done

---
+++ CHAPTER - 4/3/working-with-part-of-list.py
@@ -0,0 +1,33 @@
+'''
+=
+================================================================================================
+
+WORKING WITH PART OF LIST ======================
+
+=============================================================================================
+
+'''
+
+'''::::::::::::::::::::::::::::::: SLICING A LIST ::::::::::::::::::::::::::::::::::'''
+
+players = ['Rishabh', 'Priyanshu', 'Manish', 'Vikash', 'Rishu']
+
+'''WE CAN GENERATE ANY SLICE OF LISTS BY DOING THIS ============== '''
+
+print(players[0:3])
+
+players = ['Rishabh', 'Priyanshu', 'Manish', 'Vikash', 'Rishu']
+
+print(players[:5])
+
+players = ['Rishabh', 'Priyanshu', 'Manish', 'Vikash', 'Rishu']
+
+print(players[2:])
+
+
+''' This will print the last three element of an array '''
+
+print('This will print the last three element of an array')
+players = ['Rishabh', 'Priyanshu', 'Manish', 'Vikash', 'Rishu']
+
+print(players[-3:])
