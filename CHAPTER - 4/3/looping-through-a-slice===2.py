23e578b2f78c63b1589f4cb51b27a3047e5968fe
rishabh22204901 <rishabh22204901@gmail.com>
2023-08-25 15:42:38 +0530

    looping through a slice done

--- CHAPTER - 4/3/looping-through-a-slice===2.py
+++ CHAPTER - 4/3/looping-through-a-slice===2.py
@@ -3,3 +3,9 @@
 ==========================================================='''
 
 # now, we will use loops to print the name of first three players
+
+players = ['Rishabh', 'priyanshu', 'Manish', 'Vikash', 'Rishu']
+
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
